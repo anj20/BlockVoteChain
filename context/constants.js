95cd3ef1e1fe336b3843f38d7d7db1197d9b5242
Anurag Narayan Jena <anj20@Anj-MacBook-Air.local>
2023-07-14 16:19:08 +0530

    Configured env🔑

--- context/constants.js
+++ context/constants.js
@@ -1,4 +1,4 @@
 import voting from "./Create.json";
 
-export const VotingAddress = "0x372817e475a2E6bE66552d75De1c973a4d66E171";
+export const VotingAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS;
 export const VotingAbi = voting.abi;
