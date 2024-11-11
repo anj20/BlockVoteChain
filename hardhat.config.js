48a7c030a1b052975788a65e8c18f1f32d4513ad
anj20 <jenaanuragnarayan@gmail.com>
2024-11-11 22:32:59 +0530

    minor fix

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 const ALCHEMY_API_KEY = "LYl532JVFJWY7P6-2PDUs_XmNbykS4pY";
 const SEPOLIA_ALCHEMY_API_KEY = "MZ_n9sulIb0O2ftWEPlYuA46TmJwNSjj";
+
 const PRIVATE_KEY =
   "ebe102065928d0ae18fc5f6bc5953f7048d733a1f1fc8707babe59e8c923661b";
 module.exports = {
@@ -13,6 +14,7 @@ module.exports = {
     sepolia: {
       url: `https://eth-sepolia.g.alchemy.com/v2/${SEPOLIA_ALCHEMY_API_KEY}`,
       accounts: [`0x${PRIVATE_KEY}`],
+      chainId: 11155111,
     },
   },
 };
